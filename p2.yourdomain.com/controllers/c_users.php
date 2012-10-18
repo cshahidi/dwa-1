b7e969caa603c4a0651065a58de43b02badb993f
Susan Buck <susan@susanbuck.net>
2012-10-18 04:34:12 -0400

    Core update.

--- p2.yourdomain.com/controllers/c_users.php
+++ p2.yourdomain.com/controllers/c_users.php
@@ -7,10 +7,10 @@ class users_controller extends base_controller {
 		parent::__construct();
 	}
 
-	public function index() {
-		echo "Welcome to the Users department";
-	}
 
+	/*-------------------------------------------------------------------------------------------------
+	
+	-------------------------------------------------------------------------------------------------*/
 	public function signup() {
 		
 		# Set up template
@@ -21,18 +21,26 @@ class users_controller extends base_controller {
 		
 	}
 	
+	
+	/*-------------------------------------------------------------------------------------------------
+	
+	-------------------------------------------------------------------------------------------------*/
 	public function p_signup() {
 	
 		# What data was submitted
 		//print_r($_POST);
 		
+		# Encrypt password
 		$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
+		
+		# Create and encrypt token
+		$_POST['token']    = sha1(TOKEN_SALT.$_POST['email'].Utils::generate_random_string());
+		
+		# Store current timestamp 
 		$_POST['created']  = Time::now(); # This returns the current timestamp
 		$_POST['modified'] = Time::now();
 		
-		$_POST['token'] = sha1(TOKEN_SALT.$_POST['email'].Utils::generate_random_string());
-		
-		# Put the data in the database
+		# Insert 
 		DB::instance(DB_NAME)->insert('users', $_POST);
 		
 		echo "You're registered! Now go <a href='/users/login'>login</a>";
@@ -40,6 +48,9 @@ class users_controller extends base_controller {
 	}
 	
 	
+	/*-------------------------------------------------------------------------------------------------
+	
+	-------------------------------------------------------------------------------------------------*/
 	public function login() {
 	
 		# Load the template
@@ -50,9 +61,16 @@ class users_controller extends base_controller {
 		
 	}
 	
+	
+	/*-------------------------------------------------------------------------------------------------
+	
+	-------------------------------------------------------------------------------------------------*/
 	public function p_login() {
 	
 		$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
+		
+		# Prevent SQL injection attacks
+		$_POST = DB::instance(DB_NAME)->sanitize($_POST);
 	
 		$q = "SELECT token
 			FROM users
@@ -76,18 +94,26 @@ class users_controller extends base_controller {
 	}
 	
 	
+	/*-------------------------------------------------------------------------------------------------
+	
+	-------------------------------------------------------------------------------------------------*/
 	public function logout() {
 		echo "display the logout page";
 	}
 	
+	
+	/*-------------------------------------------------------------------------------------------------
+	
+	-------------------------------------------------------------------------------------------------*/
 	public function profile($user_name = NULL) {
 		
+		# Not logged in
 		if(!$this->user) {
 			echo "Members only. <a href='/users/login/'>Please login.</a>";
 			return;
 		}
-		
-							
+						
+		# Logged in	
 		if($user_name == NULL) {
 			echo "You did not specify a user";
 		} else {
@@ -95,16 +121,9 @@ class users_controller extends base_controller {
 			# Setup the view
 				$this->template->content = View::instance("v_users_profile");
 				$this->template->title   = "Profile for ".$user_name;
-			
-			# Set up client files
-				$client_files = Array(
-					"/css/users.css", "/js/users.js"
-					);
-				$this->template->client_files = Utils::load_client_files($client_files);
-				
-			# Pass variables to the view
-				$this->template->content->user_name = $user_name;
-			
+						
+			# Don't need to pass any variables to the view because all we need is $user and that's already set globally in c_base.php
+							
 			# Render the view
 				echo $this->template;
 
