eb2f636dea6601567b97aceb2abcbe70c3e91d27
Susan Buck <susan@susanbuck.net>
2012-10-24 23:28:48 -0400

    Work from Class 10/24

--- p2.yourdomain.com/controllers/c_users.php
+++ p2.yourdomain.com/controllers/c_users.php
@@ -67,40 +67,60 @@ class users_controller extends base_controller {
 	-------------------------------------------------------------------------------------------------*/
 	public function p_login() {
 	
-		$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
-		
 		# Prevent SQL injection attacks
 		$_POST = DB::instance(DB_NAME)->sanitize($_POST);
 	
+		# Encrypt the password		
+		$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
+			
+		# Look for a matching email and password in the DB - retrieve token if we find it		
 		$q = "SELECT token
 			FROM users
 			WHERE email = '".$_POST['email']."'
 			AND password = '".$_POST['password']."'
 			";
+			
+		echo $q;
 				
 		$token = DB::instance(DB_NAME)->select_field($q);
 		
 		# Login failed
 		if($token == "") {
-			Router::redirect("/users/login");
+			//Router::redirect("/users/login");
 		}
 		# Login passwed
 		else {
 			setcookie("token", $token, strtotime('+2 weeks'), '/');
-			
 			Router::redirect("/");
 		}
 	
 	}
 	
 	
+	
+	
 	/*-------------------------------------------------------------------------------------------------
 	
 	-------------------------------------------------------------------------------------------------*/
 	public function logout() {
-		echo "display the logout page";
-	}
 	
+		# Generate and save a new token for next login
+		$new_token = sha1(TOKEN_SALT.$this->user->email.Utils::generate_random_string());
+		
+		# Create the data array we'll use with the update method
+		# In this case, we're only updating one field, so our array only has one entry
+		$data = Array("token" => $new_token);
+		
+		# Do the update
+		DB::instance(DB_NAME)->update("users", $data, "WHERE token = '".$this->user->token."'");
+		
+		# Delete their token cookie - effectively logging them out
+		setcookie("token", "", strtotime('-1 year'), '/');
+		
+		echo "You have been logged out.";
+
+	}
+
 	
 	/*-------------------------------------------------------------------------------------------------
 	
